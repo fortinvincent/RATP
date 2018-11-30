e681145091a2c7a39ac9c3e064004ce1d01c0853
Vincent Loirat <vincentloirat@MacBook-Pro-de-Zengularity.local>
2018-11-30 14:25:51 +0100

    font added

--- src/Trafic.tsx
+++ src/Trafic.tsx
@@ -43,20 +43,28 @@ export class Trafic extends React.PureComponent<{}, IState> {
 
   public render(): JSX.Element {
     const myData = this.state.stationTrafic.map(station => 
-      ({id: '00036', y: station.trafic, x: station.rang})
+      ({id: '00036', y: station.trafic, x: station.rang })
     )
 
     return (
       <div>
-        <XYPlot
-          margin={{left: 75}}
-          width={600}
-          height={600}
-        >
-          <VerticalBarSeries className="vertical-bar-series-example" data={myData} />
-          <XAxis />
-          <YAxis marginLeft={100}/>
-        </XYPlot>
+        <div className="title">
+          Graphique de frequentation des stations de metro parisien
+        </div>
+        <div>
+          <XYPlot
+            margin={{left: 75}}
+            width={1600}
+            height={600}
+          >
+            <VerticalBarSeries className="vertical-bar-series-example" data={myData} />
+            <XAxis />
+            <YAxis marginLeft={100}/>
+          </XYPlot>
+        </div>
+        <div className="legende">
+          Source RATP 2017
+        </div>
       </div>
     )
   }
