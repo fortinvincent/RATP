67ae10c9993e78803fd1cd2d37adcdbef4e32509
Nicholas Greenway <nicholas.greenway@fabernovel.com>
2018-11-30 14:14:09 +0100

    done

--- src/Trafic.tsx
+++ src/Trafic.tsx
@@ -1,28 +1,28 @@
 import * as React from 'react';
-import {
-  XYPlot,
-  XAxis,
-  YAxis,
-  VerticalBarSeries
-} from 'react-vis';
+import { CanvasJSChart } from './libs/canvasjs.react'
 
 import './App.css'
 
 interface IState {
   readonly stationTrafic: IStationDetail[]
+  readonly stationSlice: number
+  readonly stationReseau: string
 }
 
 interface IStationDetail {
   readonly station: string
   readonly trafic: number
   readonly rang: number
+  readonly reseau: string
 }
 
 export class Trafic extends React.PureComponent<{}, IState> {
   constructor(props: {}) {
     super(props)
     this.state = {
-      stationTrafic: []
+      stationTrafic: [],
+      stationSlice: 10,
+      stationReseau: 'Métro',
     }
   }
 
@@ -31,32 +31,42 @@ export class Trafic extends React.PureComponent<{}, IState> {
     fetch('https://data.ratp.fr/api/v2/catalog/datasets/trafic-annuel-entrant-par-station-du-reseau-ferre-2017/exports/json').then(
       result => result.json().then(
         (stationTrafic: IStationDetail[]) => this.setState({
-          stationTrafic: stationTrafic.map(station => ({
-              station: station.station,
-              rang: station.rang,
-              trafic: station.trafic,
-            })
+          stationTrafic: stationTrafic.map(({ station, rang, trafic, reseau }) => ({ station, rang, trafic, reseau })
           )
         })
       ))
   }
 
   public render(): JSX.Element {
-    const myData = this.state.stationTrafic.map(station => 
-      ({id: '00036', y: station.trafic, x: station.rang})
-    )
+    const myData = this.state.stationTrafic
+    .filter(station => station.reseau === this.state.stationReseau)
+    .slice(0, this.state.stationSlice)
+    .sort((a, b) => a.rang - b.rang)
+    .map(station => (
+      {label: station.station, y: station.trafic}))
+    const uniqueValues = Array.from(new Set(myData))
+
+    const options = {
+      animationEnabled: true,
+      theme: "light1",
+      title: { text: "Test" },
+      data: [{ type: "column", dataPoints: myData }]
+    }
+
 
     return (
       <div>
-        <XYPlot
-          margin={{left: 75}}
-          width={600}
-          height={600}
-        >
-          <VerticalBarSeries className="vertical-bar-series-example" data={myData} />
-          <XAxis />
-          <YAxis marginLeft={100}/>
-        </XYPlot>
+        <input
+          type="number"
+          title="Nombre de stations à afficher"
+          value={this.state.stationSlice}
+          max={300}
+          min={1}
+          onChange={v => this.setState({stationSlice: Number(v.target.value)}) }
+        />
+        <button onClick={() => this.setState({stationReseau: 'RER'}) }> RER </button>
+        <button onClick={() => this.setState({stationReseau: 'Métro'}) }> Métro </button>
+        <CanvasJSChart options={options}/>
       </div>
     )
   }
